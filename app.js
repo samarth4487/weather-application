537895ef7f1197d78421d6610eaaf7094e370d1c
Samarth Paboowal <samarthpaboowal@Samarths-MacBook-Air.local>
2017-01-11 16:04:11 +0530

    Added functionality for getting weather data for a single city

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+var request = require('request');
 var url = 'http://api.openweathermap.org/data/2.5/weather?q=Jaipur&appid=582934f0e2a6be45ea54600ee74c3cbe&units=metric';
+
+request({
+  url: url,
+  json: true
+}, function (error, response, body) {
+  if (error) {
+    console.log('Unable to fetch weather data!');
+  } else {
+    console.log('It\'s ' + body.main.temp + '°C in ' + body.name + '!');
+  }
+});
